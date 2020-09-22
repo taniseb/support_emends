3671a209e4b851186ddada3835778f00513753ac
Hugo Santos <hugoee@gmail.com>
2020-09-22 17:18:39 -0300

    initial config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'emmends#index'
+
+  resources :users do
+    resources :supports, only: [:new, :create, :delete]
+
+  end
+
+  resources :emendas, only: [:destroy]
+
 end
