ca35515dbfad486bb0732b8e7d3d780769f39238
Hugo Santos <hugoee@gmail.com>
2020-09-22 17:54:43 -0300

    routers and controllers and devises

--- app/controllers/emmends_controller.rb
+++ app/controllers/emmends_controller.rb
@@ -1,2 +1,17 @@
 class EmmendsController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @emmends = Emmend.all
+  end
+
+  def new
+    @emmend = Emmend.new
+  end
+
+  def create
+    @emmends = Emmend.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'emmends#index'
 
-  resources :users do
-    resources :supports, only: [:new, :create, :delete]
+  devise_for :users
 
+  resources :emmends do
+    resources :supports, only: [:create]
   end
 
-  resources :emendas, only: [:destroy]
+  resources :supports, only: [:destroy]
 
 end
