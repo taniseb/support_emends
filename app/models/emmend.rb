8fbb37cae6d803fb3b8a75274a21cb57386b79c8
Hugo Santos <hugoee@gmail.com>
2020-09-23 17:07:44 -0300

    destroy action finished

--- app/models/emmend.rb
+++ app/models/emmend.rb
@@ -1,6 +1,6 @@
 class Emmend < ApplicationRecord
   belongs_to :user
-  has_many :supports
+  has_many :supports, dependent: :destroy
   #has_many :user, through: :supports, default. Mudança para ficar mais claros os nomes.
   has_many :supporters, through: :supports, source: :supporter
 end
