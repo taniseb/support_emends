8a006f4737bf3ded22a82867bab700d46e06b331
tanise bussmann <tanisebrandao@gmail.com>
2020-09-24 15:13:45 -0300

    destroy funcionando

--- app/models/emmend.rb
+++ app/models/emmend.rb
@@ -1,6 +1,11 @@
 class Emmend < ApplicationRecord
+  validates :name, presence:true, :length => { :minimum => 5}
+  validates :project, presence:true
+  validates :thema, presence:true
+  validates :description, presence:true, :length => { :minimum => 15}
   belongs_to :user
   has_many :supports, dependent: :destroy
   #has_many :user, through: :supports, default. Mudança para ficar mais claros os nomes.
   has_many :supporters, through: :supports, source: :supporter
+
 end
