13e5489c50b4772be0f92449ab3e3abfddeca25e
tanise bussmann <tanisebrandao@gmail.com>
2020-09-25 13:01:40 -0300

    criei o botão de dar like

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,14 +1,14 @@
 class SupportsController < ApplicationController
 
-  def new
-    @support = Support.new
-  end
+  #def new
+   # @support = Support.new
+  #end
   def create
     @support = Support.new
-    @support.emmend = @emmend.id
-    @support.user_id = current_user
-      render :new
-    end
+    @support.emmend = Emmend.find(params[:emmend_id])
+    @support.user_id = current_user.id
+    @support.save
+    redirect_to user_root_path
   end
 
 end
