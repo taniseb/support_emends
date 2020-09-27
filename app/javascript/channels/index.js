176e227fb7e4ba3b29f130f69e734801cb53b1e9
Hugo Santos <hugoee@gmail.com>
2020-09-27 20:51:23 -0300

    Realizadas alterações no index, no new, no edit, no show, no _form, no index.js, no _card.scss e no _button.scss

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,18 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+
+// 1. Select all the grids of stars likes
+const clickmeStar = document.querySelectorAll(".clickable");
+
+
+const alvo = (event) => {
+  event.currentTarget.classList.toggle("active");
+};
+
+const escuta = (star) => {
+  star.addEventListener("click", alvo);
+};
+
+clickmeStar.forEach(escuta);
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@ require 'faker'
   rand(1..2).times do
     emmend = Emmend.new(
       user_id: user.id,
-      project: "Monopólio das Teles",
-      thema: "5G",
-      name: "Fora Monopólio",
-      description: "A qualidade do serviço precisa melhorar"
+      project: " #{ Faker::Number.decimal(l_digits: 1, r_digits: 3)}/2020 ",
+      thema: 'Reformulação da legislação de concessões e Parcerias Público Privadas',
+      # ['Campanha Eleitoral de 2020', 'Reforma Administrativa', 'Privatização das Estatais', 'Queimadas no Pantanal', 'Volta às aulas durante à pandemia', 'Alta nos preços dos alimentos',  'Monopólio das Telecomunicações','5G', 'Meio Ambiente', 'Saúde', 'Segurança Pública', 'Reforma Tributária', 'Prisão em 2º Instância', 'Regulamentação da Regra de Ouro', 'Educação Domiciliar', 'Autonomia do Banco Central', 'Nova Lei de Falências', 'Novo marco legal do mercado de câmbio e capitais', 'Foro Privilegiado', 'PEC Paralela da Previdência', 'Novo marco regulatório do saneamento básico', 'Plano de Equilíbrio Fiscal dos estados', 'MP da Mobilidade Urbana', 'Nova Lei de Licenciamento Ambiental', 'Programa Verde e Amarelo', 'Reformulação da legislação de concessões e Parcerias Público Privadas', 'Distribuição da Vacina do Coronavírus']
+      name: "Concessões e PPP no setor de aviação",
+      description: "O setor de aviação necessita da realização de concessões e das PPPs"
       )
     emmend.save
     Support.create(
