e7c29ebc262b2d6913384e32aa4e5549d1bb4838
tanise bussmann <tanisebrandao@gmail.com>
2020-09-28 15:13:24 -0300

    seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :emmends
-  has_many :supports
+  has_many :emmends, dependent: :destroy
+  has_many :supports, dependent: :destroy
 
 end
